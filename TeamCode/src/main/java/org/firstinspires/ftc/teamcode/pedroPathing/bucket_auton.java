ba901d66f4313edcace787926b0f33dc43dc8fe1
Skyblockforlige <mr.maahir.shah@gmail.com>
2025-08-23 15:24:01 -0700

    help

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/bucket_auton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/bucket_auton.java
@@ -35,26 +35,22 @@ public class bucket_auton extends OpMode {
     private int pathState;
     private final Pose startPose = new Pose(10, 110, Math.toRadians(270));
     private final Pose dep_1 = new Pose(10, 118, Math.toRadians(270));
-    private final Pose pick2_pos = new Pose(29, 112, Math.toRadians(180));
-
+    private final Pose pick2_pos = new Pose(31, 112, Math.toRadians(180));
     private final Pose Dep_2 = new Pose(20,124, Math.toRadians(315));
-
-    private final Pose pick3_pos =new Pose(29,126, Math.toRadians(180));
-
+    private final Pose pick3_pos = new Pose(31,126, Math.toRadians(180));
     private final Pose pick4_pos = new Pose(48, 124, Math.toRadians(270));
+    private final Pose pick4_back = new Pose(48,120, Math.toRadians(270));
 
     private PathChain score1;
     private Path pick2;
     private Path score2;
-
     private Path pick3;
     private Path score3;
-
     private Path pick4;
     private Path score4;
+    private Path move4;
 
     public void buildPaths() {
-        // ---- Original paths ----
         score1 = follower.pathBuilder()
                 .addPath(new BezierLine(new Point(startPose), new Point(dep_1)))
                 .setConstantHeadingInterpolation(Math.toRadians(270))
@@ -63,23 +59,23 @@ public class bucket_auton extends OpMode {
         pick2 = new Path(new BezierLine(new Point(dep_1), new Point(pick2_pos)));
         pick2.setLinearHeadingInterpolation(dep_1.getHeading(), pick2_pos.getHeading());
 
-
         score2 = new Path(new BezierLine(new Point(pick2_pos), new Point(Dep_2)));
         score2.setLinearHeadingInterpolation(pick2_pos.getHeading(), Dep_2.getHeading());
 
         pick3 = new Path(new BezierLine(new Point(Dep_2), new Point(pick3_pos)));
         pick3.setLinearHeadingInterpolation(Dep_2.getHeading(), pick3_pos.getHeading());
+
         score3 = new Path(new BezierLine(new Point(pick3_pos), new Point(Dep_2)));
         score3.setLinearHeadingInterpolation(pick3_pos.getHeading(), Dep_2.getHeading());
 
         pick4 = new Path(new BezierLine(new Point(Dep_2), new Point(pick4_pos)));
         pick4.setLinearHeadingInterpolation(Dep_2.getHeading(), pick4_pos.getHeading());
 
+        move4 = new Path(new BezierLine(new Point(pick4_back), new Point(Dep_2)));
+        move4.setLinearHeadingInterpolation(pick4_back.getHeading(), Dep_2.getHeading());
+
         score4 = new Path(new BezierLine(new Point(pick4_pos), new Point(Dep_2)));
         score4.setLinearHeadingInterpolation(pick4_pos.getHeading(), Dep_2.getHeading());
-
-
-
     }
 
     public void autonomousPathUpdate() {
@@ -87,7 +83,7 @@ public class bucket_auton extends OpMode {
             case 0:
                 follower.followPath(score1);
                 if (pathTimer.getElapsedTimeSeconds() > 0.1) {
-                    target = -830;
+                    target = -870;
                     setPathState(1);
                 }
                 break;
@@ -96,7 +92,7 @@ public class bucket_auton extends OpMode {
                 if (!follower.isBusy())
                     clawservo.setPosition(open_pos);
 
-                if (pathTimer.getElapsedTimeSeconds()>5) {
+                if (pathTimer.getElapsedTimeSeconds() > 2) {
                     follower.followPath(pick2);
                     setPathState(2);
                 }
@@ -104,81 +100,116 @@ public class bucket_auton extends OpMode {
 
             case 2:
                 if (pathTimer.getElapsedTimeSeconds() > 0.5) {
-                    target = -1500;
+                    target = -1550;
                 }
                 if (pathTimer.getElapsedTimeSeconds() > 0.8)
                     setPathState(3);
                 break;
+
             case 3:
                 if (!follower.isBusy()) {
-                    clawservo.setPosition(close_pos);
-                    if(pathTimer.getElapsedTimeSeconds()>3)
-                    if(clawservo.getPosition()==0.125) {
-                        target = -830;
-                        if (pathTimer.getElapsedTimeSeconds() > 5) {
+                    if (Math.abs(arm.getCurrentPosition() - (-1550)) < 25) {
+                        clawservo.setPosition(close_pos);
+                    }
+                    if (pathTimer.getElapsedTimeSeconds() > 5) {
+                        target = -870;
+
+                        if (pathTimer.getElapsedTimeSeconds() > 7) {
                             follower.followPath(score2);
                             setPathState(4);
                         }
                     }
                 }
                 break;
+
             case 4:
                 if (!follower.isBusy()) {
-                    clawservo.setPosition(open_pos);
-                    if (clawservo.getPosition() == 0.4) {
-                        follower.followPath(pick3);
-
-                        if (pathTimer.getElapsedTimeSeconds() > 2) {
-                            target = -1500;
+                    if (Math.abs(arm.getCurrentPosition() - (-870)) < 25) {
+                        clawservo.setPosition(open_pos);
+                        if (pathTimer.getElapsedTimeSeconds() > 4) {
+                            follower.followPath(pick3);
                             setPathState(5);
                         }
                     }
                 }
                 break;
+
             case 5:
-                if (!follower.isBusy()) {
-                    clawservo.setPosition(close_pos);
-                    if (pathTimer.getElapsedTimeSeconds() > 3) {
-                        target = -830;
-                        if (pathTimer.getElapsedTimeSeconds() > 3) {
-                                follower.followPath(score3);
-                                setPathState(6);
-                            }
-                        }
+                if(pathTimer.getElapsedTimeSeconds()>5) {
+                    target = -1550;
+                    setPathState(6);
                 }
                 break;
+
             case 6:
                 if (!follower.isBusy()) {
-                    clawservo.setPosition(open_pos);
-                    if (clawservo.getPosition() == 0.4) {
-                        target = -1500;
-                        follower.followPath(pick4);
-                        twist.setPosition(middle);
-                        setPathState(7);
-                    }
-                }
-                break;
-            case 7:
-                if (!follower.isBusy()) {
-                    clawservo.setPosition(close_pos);
-                    if (pathTimer.getElapsedTimeSeconds()>2) {
-                        target = -830;
-                        if (pathTimer.getElapsedTimeSeconds() > 1 ) {
-                            follower.followPath(score4);
-                            setPathState(8);
+                    if (Math.abs(arm.getCurrentPosition() - (-1550)) < 25) {
+                        clawservo.setPosition(close_pos);
+                        if (pathTimer.getElapsedTimeSeconds() > 5) {
+                            target = -870;
                         }
-                    }
-                }
-                break;
-            case 8:
-                if (!follower.isBusy()) {
-                    clawservo.setPosition(open_pos);
-                    if (clawservo.getPosition() == 0.4) {
-                        setPathState(-1);
+                            if (pathTimer.getElapsedTimeSeconds() > 6) {
+                                follower.followPath(score3);
+                                setPathState(7);
+
+                        }
                     }
                 }
                 break;
 
+            case 7:
+                if (!follower.isBusy()) {
+                    if (Math.abs(arm.getCurrentPosition() - (-870)) < 25) {
+                        clawservo.setPosition(open_pos);
+                    }
+                    if (pathTimer.getElapsedTimeSeconds() > 3) {
+                        follower.followPath(pick4);
+                        setPathState(8);
+                    }
+                }
+                break;
+
+            case 8:
+                if (pathTimer.getElapsedTimeSeconds()>1.5) {
+                    target = -1550;
+                    twist.setPosition(middle);
+                    if (!follower.isBusy()) {
+                        if (Math.abs(arm.getCurrentPosition() - (-1550)) < 25) {
+                            clawservo.setPosition(close_pos);
+                            if (pathTimer.getElapsedTimeSeconds() > 1) {
+                                follower.followPath(move4);
+
+                                setPathState(9);
+                            }
+                        }
+                    }
+                }
+                break;
+
+            case 9:
+                if (pathTimer.getElapsedTimeSeconds()>1) {
+                    target = -870;
+                    setPathState(11);
+                }
+                break;
+
+            case 11:
+                if (!follower.isBusy()) {
+                    follower.followPath(score4);
+                    if (!follower.isBusy()&Math.abs(arm.getCurrentPosition() - (-870)) < 25) {
+                        clawservo.setPosition(open_pos);
+                        if (pathTimer.getElapsedTimeSeconds() > 1) {
+                            setPathState(12);
+                        }
+                    }
+                }
+                break;
+
+            case 12:
+                if (!follower.isBusy()) {
+                    setPathState(-1);
+                }
+                break;
         }
     }
 
@@ -213,8 +244,7 @@ public class bucket_auton extends OpMode {
     }
 
     @Override
-    public void init_loop() {
-    }
+    public void init_loop() {}
 
     @Override
     public void start() {
